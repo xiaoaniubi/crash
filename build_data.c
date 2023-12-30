535bf957d645cdd09fd75ad6d4f51d0448e617ed
xiaoa <gaoxiang19870307@163.com>
2023-12-30 16:27:29 +0800

    submit src

---
+++ build_data.c
@@ -0,0 +1,5 @@
+char *build_command = "crash";
+char *build_data = "Sat Dec 30 16:02:48 CST 2023 by uid=1000(xiaoa) on DESKTOP-N4CD2CK";
+char *build_target = "ARM64";
+char *build_version = "8.0.3++";
+char *compiler_version = "gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
